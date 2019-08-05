6ae96d78f631988f063fa36c904c717a820d410c
Innocent Abednego <innexsystem1@gmail.com>
2019-08-05 11:26:44 -0700

    add books resources

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Book;
+
+class BooksController extends Controller
+{
+   public function store()
+   {
+       $data = request()->validate([
+           'title' => 'required',
+           'author' => '',
+        ]);
+
+     Book::create($data);
+   }
+}
